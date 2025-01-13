61e17b9dcaf8db1c1a0a480e494d3f46324f7991
ChipRealz <21110247@student.hcmute.edu.vn>
2025-01-13 17:51:06 +0700

    baitap02

--- App.tsx
+++ App.tsx
@@ -1,118 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect, useState} from "react";
+import { SplashScreen }  from "./src/screens";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthNavigator from "./src/navigators/AuthNavigator";
+import MainNavigator from "./src/navigators/MainNavigator";
+import { StatusBar } from "react-native"; 
+import { useAsyncStorage } from "@react-native-async-storage/async-storage";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  // su dung useState de luu thoi gian 1.5s
+  const [isShowSplash, setIsShowSplash] = useState(true); 
+  //muon luu lien thi dung store redux toolkit
+  const [accessToken, setAccessToken] = useState('');
+  //kiem tra dang nhap
+  const { getItem, setItem } = useAsyncStorage('accessToken');
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsShowSplash(false);
+    }, 1500);
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
+//chay 1 lan thi dung useEffect khong tham so []
+useEffect(() => {
+  checkLogin();
+}, []);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const checkLogin = async () => {
+  const token = await getItem();
+  console.log('token', token);
+  //check token
+  token && setAccessToken(token);
+};
+  //dung dau ! de phu dinh dieu kien
+  // bg nam duoi thanh status bar
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  return <>
+  <StatusBar barStyle={'dark-content'} translucent backgroundColor={'transparent'} /> 
+  {
+    !isShowSplash ? (
+      <SplashScreen />
+    ) : (
+      <NavigationContainer>
+        {/* <AuthNavigator /> kiem tra token neu co thi tra ve Main khong thi tra ve Auth */}
+        {accessToken ? <MainNavigator /> : < AuthNavigator /> }
+      </NavigationContainer>
+    )
+  }
+  </>
+};
 
 export default App;
